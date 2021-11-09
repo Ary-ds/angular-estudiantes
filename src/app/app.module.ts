ca156179998db0dee7d465346ea2d831b5fb6426
iven ari <ivenari.dacruz@versia.com>
2021-11-09 13:32:22 +0100

    creamos el formulario para registrar una estudiante  y tambien creamos las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,37 @@ import { EstudiantesComponent } from './estudiantes/estudiantes.component';
 
 //importamos el http cliente module
 import {HttpClientModule} from '@angular/common/http';
+import { FormEstudianteComponent } from './estudiantes/form-estudiante.component';
+
+
+//importamos el routes, y routesModelo
+import { Routes, RouterModule } from '@angular/router';
+
+
+//importamos formModule para poder trabajar con formulario
+import { FormsModule } from '@angular/forms';
+
+
+
+//creamos la configuracion de routas
+const routes: Routes = [
+  { path:'', redirectTo:'/estudiantes', pathMatch: 'full'},
+  { path:'estudiantes', component: EstudiantesComponent},
+  { path:'estudiantes/from', component: FormEstudianteComponent},
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EstudiantesComponent
+    EstudiantesComponent,
+    FormEstudianteComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
